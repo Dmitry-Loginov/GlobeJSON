bb21f50999655fa99a181d5f7d480343d84ec565
Dmitry-Loginov <interestingfacts223@gmail.com>
2021-05-05 12:17:30 +0300

    delete

--- php/array.php
+++
@@ -1,9 +0,0 @@
-<?php
-$day = 3;
-$lang = 'ru';
-$arr = [
-  'ru' => [1=>'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
-  'en' => [1=>'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sanday']
-];
-echo $arr[$lang][$day];
-?>
